db0a957def55b4fa0479a0f6446fef63f2db4ff9
BrahmjyotSingh <80262200+BrahmjyotSingh@users.noreply.github.com>
2021-03-31 18:31:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,37 @@
-var fixedrect,movingrect
+
+var car,wall;
+var speed,weight;
+
 function setup() {
-  createCanvas(800,400);
- fixedrect = createSprite(400, 200, 50, 50);
- fixedrect.shapeColor="red"
- movingrect = createSprite(200,100,50,50)
- movingrect.debug=true
- fixedrect.debug=true
- movingrect.shapeColor = "red"
+  createCanvas(1600,400);
+  car = createSprite(400, 200, 50, 50);
+  wall = createSprite(1200,200,60,100)
+
+speed=random(55,90)
+weight=random(400,1500)
+
+car.velocityX = speed
+
 }
 
 function draw() {
-  background(0);  
-  movingrect.x=mouseX
-  movingrect.y=mouseY
-  if(fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2
-  &&movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2
-  &&fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2
-  &&movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2
-  ){
-    fixedrect.shapeColor="blue"
-    movingrect.shapeColor = "blue"
-  }
-  else{
-    fixedrect.shapeColor="red"
-    movingrect.shapeColor = "red"
-  }
+
+  background('black');  
+if(wall.x-car.x < (car.width+wall.width)/2){
+car.velocityX=0
+var deformation=0.5 * weight * speed * speed/22509;
+if (deformation>180){
+  car.shapeColor = "red";
+  wall.shapeColor="white"
+}
+if (deformation<180 && deformation>100){
+  car.shapeColor = "yellow";
+  wall.shapeColor="white"
+}
+if (deformation>100){
+  car.shapeColor = "green";
+  wall.shapeColor="white"
+}
+}
   drawSprites();
 }
